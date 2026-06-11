c87b5ff7a7c4fb81ffbd33338a289a12297cef91
angelrb <angelramibla@gmail.com>
2026-06-11 11:43:12 +0200

    Collect PGN games and compute user win stats
    Collect parsed PGN games into a list and defer printing until after all games are read. Add a wins counter and a user variable, iterate over collected games to print each game's headers and whether the specified user won, and finally print the total wins and win percentage.

--- chesstats.py
+++ chesstats.py
@@ -3,9 +3,24 @@ import chess.pgn
 
 pgn = open("chess_games.pgn", encoding="utf-8")
 
+chess_games = []
 chess_game = chess.pgn.read_game(pgn) 
 while chess_game is not None:
-    headers = chess_game.headers
-    print(f"{headers.get("White")} vs. {headers.get("Black")}:  {headers.get("Result")}")
+    chess_games.append(chess_game)
     chess_game = chess.pgn.read_game(pgn) 
-    
+
+wins = 0
+user = "TensiKReyDama"
+
+for cg in chess_games:
+    hd = cg.headers
+    print(f"{hd.get("White")} vs. {hd.get("Black")}:  {hd.get("Result")}")
+    if (user in hd.get("White", "?") and hd.get("Result") == "1-0") or (user in hd.get("Black", "?") and hd.get("Result") == "0-1"):
+        print(user + " won.")
+        wins += 1
+    else:
+        print(user + " did not win.")
+
+    print()
+
+print(f"{user} won {wins} games out of {len(chess_games)}, making a win % of: {(wins / len(chess_games)) * 100} %")
